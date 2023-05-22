5e62994d9ec1dd06c5bbcae2a9c4944dc304d944
rehan-onyxtec <mr@onyxtec.co>
2023-05-22 10:09:08 +0500

    user mail verification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -51,8 +52,10 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'regex:/^[^@]+@onyxtec\.co$/i'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ],[
+            'email.regex' => 'Only email addresses from onyxtec.co domain are allowed.',
         ]);
     }
 
