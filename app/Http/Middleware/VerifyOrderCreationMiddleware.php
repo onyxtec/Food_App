31981b4b68d0d46ef73c649ceb8dd823eb1507e9
rehan-onyxtec <mr@onyxtec.co>
2023-06-08 14:55:36 +0500

    add off days and do no let user order on off days and time

--- app/Http/Middleware/VerifyOrderCreationMiddleware.php
+++ app/Http/Middleware/VerifyOrderCreationMiddleware.php
@@ -3,11 +3,11 @@
 namespace App\Http\Middleware;
 
 use App\Models\OffDay;
+use App\Models\TimeSetting;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Auth;
 
 class VerifyOrderCreationMiddleware
 {
@@ -18,25 +18,27 @@ class VerifyOrderCreationMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // $current_date_time = Carbon::now();
-        $current_date_time = Carbon::parse('2023-06-16');
-        // $current_date = $current_date_time->format('Y-m-d');
+        $current_date_time = Carbon::now();
         $off_days = OffDay::all();
 
         foreach ($off_days as $off_day) {
-            // $start_date = Carbon::parse($off_day->start_date)->format('Y-m-d');
-            // $end_date = Carbon::parse($off_day->end_date)->format('Y-m-d');
             $start_date = Carbon::parse($off_day->start_date);
             $end_date = Carbon::parse($off_day->end_date);
 
             if ($current_date_time->between($start_date, $end_date)) {
 
-                // if (Auth::check()) {
-                //     Auth::logout();
-                // }
-                // return redirect()->route('login')->with('error', 'Order creation is not allowed on off days.');
-
                 return redirect()->route('home')->with('error', 'Order creation is not allowed on off days.');
+            }
+        }
+
+        $time_settings = TimeSetting::first();
+
+        if ($time_settings) {
+            $order_start_time = Carbon::parse($time_settings->order_start_time);
+            $order_end_time = Carbon::parse($time_settings->order_end_time);
+
+            if (!$current_date_time->between($order_start_time, $order_end_time)) {
+                return redirect()->route('home')->with('error', 'Order creation is not allowed during off hours.');
             }
         }
 
